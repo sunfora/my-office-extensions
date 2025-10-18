b08774a73330d3789dc4ead8e49d3002ba0d83ea
JunkJunkJunk <sandovin@mail.ru>
2025-10-18 09:34:55 +0300

    change commands to windows equivalents

--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rm build\test-plugin.mox
-cp window\build\window.dll src\bin\window.dll
+del build\test-plugin.mox
+copy window\build\window.dll src\bin\window.dll
 mox create --source=src --package=test-plugin
-mv test-plugin.mox build\test-plugin.mox
+move test-plugin.mox build\test-plugin.mox
